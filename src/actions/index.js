d4029c851c56b725dbc4ad7fee5ac3688eec67c9
Thomas Shamp <T.shamp7@gmail.com>
2020-12-16 12:36:40 -0800

    Add UserHeader component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,17 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = (response) => {
-  // const response = await jsonPlaceholder.get("/posts");
+export const fetchPosts = () => {
   return async (dispatch) => {
     const response = await jsonPlaceholder.get("/posts");
 
-    dispatch({ type: "FETCH_POSTS", payload: response });
+    dispatch({ type: "FETCH_POSTS", payload: response.data });
+  };
+};
+
+export const fetchUser = (userId) => {
+  return async (dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${userId}`);
+
+    dispatch({ type: "FETCH_USER", payload: response.data });
   };
 };
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,24 +1,40 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
-
-import jsonPlaceholder from "../apis/jsonPlaceholder";
+import UserHeader from "./UserHeader";
 
 class PostList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      posts: [],
-    };
-  }
   componentDidMount() {
-    fetchPosts();
+    this.props.fetchPosts();
+  }
+
+  renderList() {
+    return this.props.posts.map((post) => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user"></i>
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+            <UserHeader key={post.userId} userId={post.userId} />
+          </div>
+        </div>
+      );
+    });
   }
 
   render() {
-    return <div>Posts List</div>;
+    console.log(this.props.posts);
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts,
+  };
+};
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
