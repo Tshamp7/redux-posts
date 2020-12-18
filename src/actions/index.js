9e0247c399d43650c3f9ced8bae2d933ab3f2efb
Thomas Shamp <T.shamp7@gmail.com>
2020-12-18 06:54:59 -0800

    complete app

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,35 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
 
-export const fetchPosts = () => {
-  return async (dispatch) => {
+export const fetchPosts = function () {
+  return async function (dispatch) {
     const response = await jsonPlaceholder.get("/posts");
 
     dispatch({ type: "FETCH_POSTS", payload: response.data });
   };
 };
 
-export const fetchUser = (userId) => {
-  return async (dispatch) => {
-    const response = await jsonPlaceholder.get(`/users/${userId}`);
+// memoized _fetchUser must be declared outside of action call to properly memoize the call.
+export const fetchUser = (userId) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${userId}`);
 
-    dispatch({ type: "FETCH_USER", payload: response.data });
-  };
+  dispatch({ type: "FETCH_USER", payload: response.data });
 };
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
+};
+
+// memoized version is below.
+
+// const _fetchUser = _.memoize(async (userId, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${userId}`);
+
+//     dispatch({ type: "FETCH_USER", payload: response.data });
+//   });
--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -1,12 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { fetchUser } from "../actions";
 
 class PostHeader extends Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
-
   render() {
     const { user } = this.props;
 
@@ -24,4 +19,4 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export default connect(mapStateToProps, { fetchUser })(PostHeader);
+export default connect(mapStateToProps)(PostHeader);
